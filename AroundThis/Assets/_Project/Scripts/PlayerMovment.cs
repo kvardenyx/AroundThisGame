d4c35a2322b5ac4392df5a50feb3cd04f46f4e8d
Andrey <kvardenyx@gmail.com>
2025-09-07 19:41:16 +0300

    Transfer on a new Unity version

--- AroundThis/Assets/_Project/Scripts/PlayerMovment.cs
+++ AroundThis/Assets/_Project/Scripts/PlayerMovment.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 
-public class PlayerMovment : MonoBehaviour
+namespace _Project.Scripts
 {
-    public GameObject player;
-    [SerializeField] private float playerSpeed = 150f;
+    public class PlayerMovment : MonoBehaviour
+    {
+        public GameObject player;
+        [SerializeField] private float playerSpeed = 150f;
     
-    private void Start()
-    {
-        Movment();
-    }
-
-    void Update()
-    {
-        Movment();
-
-        if (Input.GetMouseButtonDown(0))
+        private void Start()
         {
-            playerSpeed = -playerSpeed;
+            Movment();
         }
-    }
 
-    void Movment()
-    {
-        player.transform.Rotate(0f, 0f, playerSpeed * Time.deltaTime);
+        void Update()
+        {
+            Movment();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                playerSpeed = -playerSpeed;
+            }
+        }
+
+        void Movment()
+        {
+            player.transform.Rotate(0f, 0f, playerSpeed * Time.deltaTime);
+        }
     }
 }
